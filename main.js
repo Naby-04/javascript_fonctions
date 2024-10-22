daf69359e6df9157fd7eb652641f08daf0910e64
Nabi <smohamed.diop@gmail.com>
2024-10-22 16:08:58 +0100

    reajustement

--- main.js
+++ main.js
@@ -2,13 +2,12 @@
 
 // 2- Créez une fonction appelée "addition" qui prend deux paramètres (a et b) et retourne leur somme. Testez cette fonction en lui passant deux nombres et affichez le résultat dans la console.
 
-// 3- Créez une fonction appelée "estPair" qui prend un nombre en paramètre et retourne true s'il est pair, sinon false. Testez cette fonction avec différents nombres et affichez le résultat dans la console.
-
-// 4- Créez une fonction appelée "calculerMoyenne" qui prend un tableau de nombres en paramètre et retourne leur moyenne. Testez cette fonction avec un tableau de nombres et affichez le résultat dans la console.
-
-// Créons une fonction qui additionne deux valeurs.
 function addition(a, b) {
   return a + b;
 }
 // Testons la fonction avec deux nombres.
 console.log(addition(5, 7)); // Affiche 12 dans la console.
+
+// 3- Créez une fonction appelée "estPair" qui prend un nombre en paramètre et retourne true s'il est pair, sinon false. Testez cette fonction avec différents nombres et affichez le résultat dans la console.
+
+// 4- Créez une fonction appelée "calculerMoyenne" qui prend un tableau de nombres en paramètre et retourne leur moyenne. Testez cette fonction avec un tableau de nombres et affichez le résultat dans la console.
