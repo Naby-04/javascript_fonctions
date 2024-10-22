a31e7cd702ab187c9abf2218296a64969e96dba5
Nabi <smohamed.diop@gmail.com>
2024-10-22 14:19:04 +0100

    initialisation

---
+++ main.js
@@ -0,0 +1,7 @@
+// Exercice 2 : Les fonctions
+
+// 2- Créez une fonction appelée "addition" qui prend deux paramètres (a et b) et retourne leur somme. Testez cette fonction en lui passant deux nombres et affichez le résultat dans la console.
+
+// 3- Créez une fonction appelée "estPair" qui prend un nombre en paramètre et retourne true s'il est pair, sinon false. Testez cette fonction avec différents nombres et affichez le résultat dans la console.
+
+// 4- Créez une fonction appelée "calculerMoyenne" qui prend un tableau de nombres en paramètre et retourne leur moyenne. Testez cette fonction avec un tableau de nombres et affichez le résultat dans la console.
